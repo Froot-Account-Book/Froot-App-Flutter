3b851879cd5dfad33910aed22b799745397c2514
zihasoo <dlquddbs-@daum.net>
2023-03-29 16:32:38 +0900

    init

---
+++ android/app/src/main/kotlin/com/NL/froot_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.NL.froot_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(child: Scaffold(
+        body: Center(child: Text("Froot", style: TextStyle(fontSize: 35)),),
+      )),
+    );
+  }
+}
